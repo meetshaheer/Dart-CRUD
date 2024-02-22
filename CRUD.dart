d3c0fc024a41e142f38759244859ec6e84753118
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-02-22 12:04:57 +0500

    Creating Main Menu for CRUD Operations

--- CRUD.dart
+++ CRUD.dart
@@ -1,3 +1,16 @@
+import 'dart:io';
+
 void main(List<String> args) {
-  
+  List Data = ["Shaheer", 23, "MS", "Last Year"];
 }
+
+void menu() async {
+  print("Please Wait! Loading ........\n");
+  await Future.delayed(Duration(seconds: 3));
+  print("Press 1. For View");
+  print("Press 2. For Add");
+  print("Press 3. For Delete");
+  print("Press 4. For Update");
+}
+
+
